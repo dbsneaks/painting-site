fed6c3816a4f7ab3443268743be5b5000e35cbe7
Dana Brook <danabrook@MacBook-Pro-2.local>
2026-04-27 09:55:01 -0700

    Fix auth function for Decap CMS

--- api/auth.js
+++ api/auth.js
@@ -1,36 +1,46 @@
-const fetch = require('node-fetch');
-
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   const { code } = req.query;
 
-  const response = await fetch('https://github.com/login/oauth/access_token', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Accept': 'application/json'
-    },
-    body: JSON.stringify({
-      client_id: process.env.OAUTH_CLIENT_ID,
-      client_secret: process.env.OAUTH_CLIENT_SECRET,
-      code
-    })
-  });
+  if (!code) {
+    return res.status(400).send('Missing code parameter');
+  }
 
-  const data = await response.json();
+  try {
+    const response = await fetch('https://github.com/login/oauth/access_token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        client_id: process.env.OAUTH_CLIENT_ID,
+        client_secret: process.env.OAUTH_CLIENT_SECRET,
+        code
+      })
+    });
 
-  res.send(`
-    <html>
-      <body>
-        <script>
-          if (window.opener) {
-            window.opener.postMessage(
-              'authorization:github:success:${JSON.stringify(data)}',
-              window.location.origin
-            );
-          }
-          window.close();
-        </script>
-      </body>
-    </html>
-  `);
-};
+    const data = await response.json();
+
+    if (data.error) {
+      return res.status(401).json({ error: data.error });
+    }
+
+    res.send(`
+      <html>
+        <body>
+          <script>
+            if (window.opener) {
+              window.opener.postMessage(
+                'authorization:github:success:${JSON.stringify(data)}',
+                window.location.origin
+              );
+            }
+            window.close();
+          </script>
+        </body>
+      </html>
+    `);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
